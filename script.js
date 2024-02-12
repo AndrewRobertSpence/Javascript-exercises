3654cb82f2416470955f3a1abda8d1c8f4dcf4c5
Andrew Spence <aspence3595@gmail.com>
2024-02-13 12:01:48 +1300

    completed the guessing game

---
+++ script.js
@@ -0,0 +1,17 @@
+let favouriteFood = "Thai";
+console.log (favouriteFood);
+
+let age = "49";
+if (age => 18) {
+    console.log("I'm an adult");
+}
+else {console.log("I'm a young person")};
+
+let secretNumber = "32";
+let guessNumber = prompt("Guess the secret number: ");
+
+if (secretNumber !== guessNumber){
+    console.log("Your guess was wrong");
+} else {
+    console.log("Congratulations, you are correct!");
+}
