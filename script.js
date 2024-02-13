e04629827eccf5840c348af9c8c8cb4d35575210
Andrew Spence <aspence3595@gmail.com>
2024-02-14 10:10:33 +1300

    completed the first 2 intermediate examples

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let favouriteFood = "Thai";
-console.log (favouriteFood);
+console.log(favouriteFood);
 
 let age = "49";
-if (age => 18) {
-    console.log("I'm an adult");
+if ((age) => 18) {
+  console.log("I'm an adult");
+} else {
+  console.log("I'm a young person");
 }
-else {console.log("I'm a young person")};
 
 // let secretNumber = "32";
 // let guessNumber = prompt("Guess the secret number: ");
@@ -20,5 +21,33 @@ else {console.log("I'm a young person")};
 // let numberTwo = prompt("select another number: ");
 // console.log(Number(oneNumber) + Number(numberTwo));
 
-let temperature = "22";
-if ()
+// let temperature = 27;
+// if (temperature <= 16) {
+//   console.log("it's cold today");
+// } else if (temperature >= 17 && temperature <= 26) {
+//   console.log("it is warm today");
+// } else {
+//   console.log("it is hot today");
+// }
+
+// let monthlyIncome = prompt("enter monthly income");
+// let monthlyRent = prompt("enter monthly rent cost");
+// let monthlyUtilities = prompt("enter month utilities cost");
+// let monthlyFood = prompt("enter monthly food costs");
+// let totalExpenses = Number(monthlyRent) + Number(monthlyUtilities) + Number(monthlyFood)
+// let netIncome = Number(monthlyIncome) - Number(totalExpenses)
+// if (netIncome >= 0) {
+//   console.log("You are living within your budget")
+// }
+// else {
+//   console.log("You are not living within your budget")
+// }
+
+// let eventDate = prompt("Enter day of an event")
+// let userAvailability = prompt("Are you available on this day?")
+// if (userAvailability.toLowerCase() == "yes") {
+//   console.log ("Your attendance is confirmed")
+// } else {
+//   console.log ("Unfortunately you are unable to attendance")
+// }
+
