15292d0c63ec6d9671158c1317ccc43a01b35d08
Andrew Spence <aspence3595@gmail.com>
2024-02-13 13:43:17 +1300

    added basic math script

--- script.js
+++ script.js
@@ -7,11 +7,18 @@ if (age => 18) {
 }
 else {console.log("I'm a young person")};
 
-let secretNumber = "32";
-let guessNumber = prompt("Guess the secret number: ");
+// let secretNumber = "32";
+// let guessNumber = prompt("Guess the secret number: ");
 
-if (secretNumber !== guessNumber){
-    console.log("Your guess was wrong");
-} else {
-    console.log("Congratulations, you are correct!");
-}
+// if (secretNumber !== guessNumber){
+//     console.log("Your guess was wrong");
+// } else {
+//     console.log("Congratulations, you are correct!");
+// }
+
+// let oneNumber = prompt("select 1 number: ")
+// let numberTwo = prompt("select another number: ");
+// console.log(Number(oneNumber) + Number(numberTwo));
+
+let temperature = "22";
+if ()
